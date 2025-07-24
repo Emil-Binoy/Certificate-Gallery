4af708fb3e9882480603d83742dc63c7870b7376
Emil Binoy <emilbinoy06@gmail.com>
2025-07-24 16:10:02 +0530

    gallerey created

---
+++ assets/script/script.js
@@ -0,0 +1,36 @@
+let images = document.querySelectorAll('.certificate');
+
+images.forEach((img,index)=>{
+    img.addEventListener('click',()=>{
+        showPopup(img.src,index);
+    });
+});
+
+function showPopup(src, selectedIndex){
+    document.getElementById('popup-img').src =src;
+    document.getElementById('popup').style.display = "block";
+    document.querySelector('.thumbnail-container').innerHTML = "";
+
+    images.forEach((img, i)=>{
+        let thumb = document.createElement("img");
+        thumb.src = img.src;
+        thumb.classList.add("thumbnail");
+
+        if(i === selectedIndex){
+            thumb.classList.add('selected');
+        }
+
+        thumb.addEventListener('click',()=>{
+            document.getElementById('popup-img').src = img.src;
+
+            document.querySelectorAll(".thumbnail").forEach(t => t.classList.remove("selected"));
+            thumb.classList.add('selected');
+        });
+
+        document.getElementById('popup-thumbnails').appendChild(thumb);
+    });
+}
+
+document.getElementById('close').addEventListener('click',()=>{
+    document.getElementById('popup').style.display= "none";
+})
